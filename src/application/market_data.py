8dbd256c27eafebac3b71135a37259db09d2f435
stejul <stejul@rocketmail.com>
2025-02-28 18:15:30 +0100

    fix: caching is now as parquet

--- src/application/market_data.py
+++ src/application/market_data.py
@@ -2,6 +2,7 @@ from src.infrastructure.data.yfinance import fetch_historical_data
 from src.domain.models import MarketData
 from src.infrastructure.logging import get_logger
 from src.infrastructure.config import get_settings
+from src.infrastructure.persistence import parquet_writer
 import pandas as pd
 import os
 import pickle
@@ -28,7 +29,7 @@ class MarketDataService:
     def _get_cache_filepath(self, symbol, start_date, end_date):
         """Generates a unique cache file path based on the request parameters."""
         hash_key = hashlib.md5(f"{symbol}_{start_date}_{end_date}".encode()).hexdigest()
-        return os.path.join(self.cache_dir, f"{hash_key}.pkl")
+        return os.path.join(self.cache_dir, f"{hash_key}.parquet")
 
     def _is_cache_valid(self, filepath):
         """Checks if the cache file is still valid based on expiration settings."""
@@ -47,8 +48,7 @@ class MarketDataService:
 
         if self.cache_enabled and self._is_cache_valid(cache_file):
             logger.info(f"Loading cached data for {symbol} from {cache_file}")
-            with open(cache_file, "rb") as f:
-                return pickle.load(f)
+            return pd.read_parquet(cache_file)
 
         logger.info(f"Fetching fresh data for {symbol} from {self.data_source}...")
         data = fetch_historical_data(symbol, start_date, end_date)
@@ -57,10 +57,11 @@ class MarketDataService:
 
         df = pd.DataFrame(data_dict, columns=["symbol", "timestamp", "open_price", "high_price", "low_price", "close_price", "volume"])
 
+        pq_writer = parquet_writer.ParquetWriter()
+
         # Store in cache if enabled
         if self.cache_enabled:
-            with open(cache_file, "wb") as f:
-                pickle.dump(df, f)
+            pq_writer.write(df, cache_file)
             logger.info(f"Cached data saved at {cache_file}")
 
         return df
